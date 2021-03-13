6ffc8cd1f6b41a0f5cfdf593d2a30cb5f616a813
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-03-13 22:26:45 +1100

    added prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var length = prompt("How long would you like the password to be? (8-128 characters)");
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid length");
+    return;
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
